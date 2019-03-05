3ee0222f4a8941cbb132ab80f45cd7930f3b1c17
albapc <alba.perez.ce@gmail.com>
2019-03-05 18:15:20 +0100

    cambios IA

--- PONG/src/pong/PONG.java
+++ PONG/src/pong/PONG.java
@@ -11,14 +11,22 @@ import javax.swing.JFrame;
  *
  * @author socamporomani
  */
-public class PONG {
+public class PONG extends JFrame {
+    private int estado = 0; //0=jugando 1=fin 2=win
+    private int toques = 0;
 
+
+    public void crearTablero(Graphics g) {
+//inicializar
+        estado = 0;
+        toques = 0;
+        }
     /**
      * @param args the command line arguments
      */
  public static void main(String[] args) {
       Ventana frame = new Ventana();
       frame.Window();
-      }
-    
+
+}
 }
--- PONG/src/pong/UNOvsIA/Start.java
+++ PONG/src/pong/UNOvsIA/Start.java
@@ -9,49 +9,28 @@ import java.awt.*;
 
 import java.awt.event.*;
 import javax.swing.JFrame;
+
 /**
  *
  * @author socamporomani
  */
-public class Start extends JFrame{
+public class Start extends JFrame {
 
-    private int estado=0; //0=jugando 1=fin 2=win
-private int toques=0;
+    private int estado = 0; //0=jugando 1=fin 2=win
+    private int toques = 0;
 
-public Start(){
- 
-}
+   
+    public Start() {
 
-public void crearTablero(){
+    }
+
+    public void crearTablero(Graphics g) {
 //inicializar
- 
-  
-     
-  estado=0;
-  toques=0;
-  
- }
+        estado = 0;
+        toques = 0;
+
+        
+}
+}
    
- 
-  
-   
-
-
-public void update(Graphics g){
-
-    paint(g);
-
-}
-
-public void paint(Graphics g){
-  
-  g.setFont(new Font("ARIAL",Font.BOLD,14));
- 
-   g.clearRect(0, 0, getWidth(), 40);
-
-}
-}
-
-
-
 
